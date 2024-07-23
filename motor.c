3649a19722a35fd1dcbcb9dab5a490d5497d21d8
Navaneeth Krishna N S <navaneeth.ns.2003@gmail.com>
2024-07-23 16:16:00 +0530

    Add files via upload

---
+++ motor.c
@@ -0,0 +1,15 @@
+#include<LPC214X.h>
+int main(){
+while(1){
+IODIR1&=(~(0x01<<16));//set direction as output
+IODIR0|=(0x01<<13);//set direction as output
+IODIR0|=(0x01<<8);
+if((0x01<<16)&IOPIN1){
+	IOSET0|=(0x01<<13);
+
+}
+	else{
+IOCLR0|=(0x01<<13);
+	}
+	}	
+}
